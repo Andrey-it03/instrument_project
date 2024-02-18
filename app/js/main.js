1057d82faf846d99d7d8545e27dd2f1f23755966
Andrey-it03 <a_ivanov50@mail.ru>
2024-02-18 17:56:15 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 $(function () {
+    $('.product__inner-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button class="product-section__slider-btn product-section__slider-btnprev"><img src="../images/icon/left-arrow.svg" alt=""></button>',
+        nextArrow: '<button class="product-section__slider-btn product-section__slider-btnnext"><img src="../images/icon/right-arrow.svg" alt=""></button>'
+    }
+    );
 
     $('.product__compare ').on('click', function () { // при нажатии
         $(this).toggleClass('product__compare--active') // toggleClass значит что при нажатии класс изменяется, а при еще одном нажатии убирается.
